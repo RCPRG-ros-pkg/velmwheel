7972180399fad1d440a8c36ab2d9d14b20a40d53
Dangield <dan.gield@gmail.com>
2023-10-31 18:24:24 +0100

    Imu can now be corrupted using GAN generator

--- src/nodes/odometry_anomaly_detector/odometry_anomaly_detector/corrupt_imu_data.py
+++ src/nodes/odometry_anomaly_detector/odometry_anomaly_detector/corrupt_imu_data.py
@@ -4,6 +4,8 @@ from sensor_msgs.msg import Imu
 from rclpy.clock import ROSClock
 from datetime import datetime, timedelta
 import random
+from odometry_anomaly_detector.train_imu_gan import make_generator_model
+import tensorflow as tf
 
 class FalseImu(Node):
 	"""docstring for FalseImu"""
@@ -13,11 +15,18 @@ class FalseImu(Node):
 		self.imu_publisher = self.create_publisher(Imu, '/velmwheel/imu/out', 10,)
 		self.fake_msg = Imu()
 		self.declare_parameter('anomaly_type', 'none')
-		self.declare_parameter('corrupt_time', 60)
+		self.declare_parameter('corrupt_time', 0)
 		self.declare_parameter('max_noise', 0.1)
+		self.declare_parameter('gan_ckpt_file', 'none')
 		self.corrupt_time = datetime.now() + timedelta(seconds = self.get_parameter('corrupt_time').get_parameter_value().integer_value)
 		# self.fake_msg.linear_acceleration.z = 9.8
 		self.lagged_message = None
+		if self.get_parameter('anomaly_type').get_parameter_value().string_value == 'gan':
+			self.generator = make_generator_model(3, 3)
+			checkpoint = tf.train.Checkpoint(generator = self.generator)
+			checkpoint.restore(self.get_parameter('gan_ckpt_file').get_parameter_value().string_value).expect_partial()
+		else:
+			self.generator = None
 
 	def msg_callback_imu(self, msg):
 		if datetime.now() > self.corrupt_time:
@@ -47,9 +56,20 @@ class FalseImu(Node):
 				self.lagged_message = None
 			self.fake_msg = msg
 			noise_val = self.get_parameter('max_noise').get_parameter_value().double_value
-			self.fake_msg.linear_acceleration.x = self.fake_msg.linear_acceleration.x + random.uniform(-noise_val, noise_val)
-			self.fake_msg.linear_acceleration.y = self.fake_msg.linear_acceleration.y + random.uniform(-noise_val, noise_val)
-			self.fake_msg.angular_velocity.z = self.fake_msg.angular_velocity.z + random.uniform(-noise_val, noise_val)
+			self.fake_msg.linear_acceleration.x = self.fake_msg.linear_acceleration.x + random.uniform(-noise_val, noise_val) * 5.45
+			self.fake_msg.linear_acceleration.y = self.fake_msg.linear_acceleration.y + random.uniform(-noise_val, noise_val) * 8.24
+			self.fake_msg.angular_velocity.z = self.fake_msg.angular_velocity.z + random.uniform(-noise_val, noise_val) * 1.76
+		elif anomaly == 'gan':
+			if not self.generator:
+				self.generator = make_generator_model(3, 3)
+				checkpoint = tf.train.Checkpoint(generator = self.generator)
+				checkpoint.restore(self.get_parameter('gan_ckpt_file').get_parameter_value().string_value).expect_partial()
+			generated = self.generator(tf.random.normal([1, 3]), training = False)
+			maximum = [1.7606009, 5.4503107, 8.242411]
+			generated = [maximum[i] * generated[0][i] for i in range(3)]
+			self.fake_msg.angular_velocity.z = float(generated[2])
+			self.fake_msg.linear_acceleration.x = float(generated[0])
+			self.fake_msg.linear_acceleration.y = float(generated[1])
 		return self.fake_msg
 
 def main():
